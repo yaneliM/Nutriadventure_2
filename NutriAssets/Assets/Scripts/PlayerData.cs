73b1c650c386e7183ebf31976ddbd1458a0c5a46
DanielNajera115 <47648242+DanielNajera115@users.noreply.github.com>
2019-11-04 21:27:36 -0800

    Add files via upload

---
+++ NutriAssets/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData 
+{
+   
+   public string name;
+   public int age;
+
+   public PlayerData(MainSrScript player){
+   		name = player.name_Input.text;
+   		age = int.Parse(player.age_Input.text);
+   }
+
+}
